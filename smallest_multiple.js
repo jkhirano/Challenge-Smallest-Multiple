8e49176071e11c18bbc52db86f7c44bf1a7076fc
Janelle K Hirano <jaahnelle@gmail.com>
2019-08-07 17:50:50 -1000

    finished

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,9 +6,28 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-module.exports = function( ceiling ) {
+module.exports = function(ceiling) {
   // do work here
 
+  let smallestNum = 1;
+  let divNum = 0;
 
-  return 0;
+  // this will end once divNum is smaller than ceiling
+  while (divNum < ceiling) {
+    divNum = 0;
+    smallestNum++;
+    console.log(smallestNum);
+
+    for (let i = 1; i <= ceiling; i++) {
+      console.log("i: " + i);
+      if (smallestNum % i === 0) {
+        divNum++;
+        console.log("divNum: " + divNum);
+      } else {
+        break;
+      }
+    }
+  }
+
+  return smallestNum;
 };
